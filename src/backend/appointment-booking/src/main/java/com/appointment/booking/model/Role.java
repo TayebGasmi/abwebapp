bb3fa4fdf405b7f92baa28933cc6b8e1f98372cb
trabelsifiras <firas.trabelsi.1@esprit.tn>
2024-07-05 11:16:34 +0200

    add roles

---
+++ src/backend/appointment-booking/src/main/java/com/appointment/booking/model/Role.java
@@ -0,0 +1,25 @@
+package com.appointment.booking.model;
+
+import com.appointment.booking.enums.Rolename;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @Column(unique = true)
+    private Rolename roleName;
+
+
+}
