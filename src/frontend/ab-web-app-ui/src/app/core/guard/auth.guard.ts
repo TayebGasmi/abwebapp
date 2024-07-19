fb6f32b487c783b90cf14e0dd2bd3b39cbb21969
guesmi <tayebgasmi1999@gmail.com>
2024-07-19 20:32:27 +0200

    register form

--- src/frontend/ab-web-app-ui/src/app/core/guard/auth.guard.ts
+++ src/frontend/ab-web-app-ui/src/app/core/guard/auth.guard.ts
@@ -1,15 +1,23 @@
 import {CanActivateFn, Router} from '@angular/router';
-import { inject } from '@angular/core';
+import {inject} from '@angular/core';
 import {AuthService} from "../service/auth.service";
+import {map, of} from "rxjs";
+import {catchError} from "rxjs/operators";
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (route,   state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-
-  if (authService.isAuthenticated()) {
-    return true;
-  } else {
-    router.navigate(['auth/login']);
-    return false;
+  if (!authService.isLoggedIn()) {
+    router.navigate(['/login']);
+    return of(false);
   }
+
+  return authService.checkTokenExpirationAndRefresh()
+  .pipe(
+    map(() => true),
+    catchError(() => {
+      router.navigate(['/login']);
+      return of(false);
+    })
+  );
 };
