b0239af7ac222c1f5a3074fc4b720daaf91aad26
guesmi <tayebgasmi1999@gmail.com>
2024-08-25 14:17:11 +0200

    session

--- src/frontend/ab-web-app-ui/src/app/modules/auth/login/login.component.ts
+++ src/frontend/ab-web-app-ui/src/app/modules/auth/login/login.component.ts
@@ -1,21 +1,20 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {InputTextModule} from 'primeng/inputtext';
-import {CheckboxModule} from 'primeng/checkbox';
-import {Router, RouterLink} from '@angular/router';
-import {AppConfigComponent} from '../../../layout/config/app.config.component';
-import {LayoutService} from '../../../layout/service/app.layout.service';
-import {ButtonDirective} from 'primeng/button';
-import {Ripple} from 'primeng/ripple';
-import {GoogleSigninButtonModule, SocialAuthService} from '@abacritt/angularx-social-login';
-import {DividerModule} from 'primeng/divider';
-import {AuthService} from '../../../core/service/auth.service';
-import {Login} from '../../../core/models/login';
-import {BackgroundComponent} from "../../../shared/components/background/background.component";
-import {map, switchMap} from "rxjs";
-import {SessionComponent} from "../../session/session.component";
-import {SessionBookLandingService} from "../../../core/service/session-book-landing.service";
-import {SessionDto} from "../../../core/models/session";
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { Router, RouterLink } from '@angular/router';
+import { AppConfigComponent } from '../../../layout/config/app.config.component';
+import { LayoutService } from '../../../layout/service/app.layout.service';
+import { ButtonDirective } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { GoogleSigninButtonModule, SocialAuthService } from '@abacritt/angularx-social-login';
+import { DividerModule } from 'primeng/divider';
+import { AuthService } from '../../../core/service/auth.service';
+import { Login } from '../../../core/models/login';
+import { BackgroundComponent } from "../../../shared/components/background/background.component";
+import { map, switchMap, Subscription } from "rxjs";
+import { SessionBookLandingService } from "../../../core/service/session-book-landing.service";
+import { SessionDto } from "../../../core/models/session";
 
 @Component({
   selector: 'app-login',
@@ -35,16 +34,18 @@ import {SessionDto} from "../../../core/models/session";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
   router = inject(Router);
   private socialAuthService = inject(SocialAuthService);
-  sessionDto!:SessionDto;
+  sessionDto!: SessionDto;
+  private subscriptions: Subscription[] = [];
+
   constructor(
     private fb: FormBuilder,
     private layoutService: LayoutService,
     private authService: AuthService,
-    private session:SessionBookLandingService
+    private session: SessionBookLandingService
   ) {
     this.loginForm = this.fb.group({
       email: [''],
@@ -57,18 +58,17 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.session.currentMessage.subscribe(session => this.sessionDto=session)
-    console.log(this.sessionDto)
-    this.socialAuthService.authState.pipe(
-      map((user) => {
-          return {
-            oauthProvider: user.provider,
-            idToken: user.idToken,
-          }
-        }
+    const sessionSubscription = this.session.currentMessage.subscribe(session => this.sessionDto = session);
+    console.log(this.sessionDto);
+
+    const authSubscription = this.socialAuthService.authState.pipe(
+      map((user) => ({
+          oauthProvider: user.provider,
+          idToken: user.idToken,
+        })
       ),
-      switchMap(user => this.authService.socialLogin(user)))
-    .subscribe({
+      switchMap(user => this.authService.socialLogin(user))
+    ).subscribe({
       next: response => {
         this.authService.addToken(response.accessToken);
         this.authService.addUser(response.user);
@@ -76,11 +76,13 @@ export class LoginComponent implements OnInit {
         this.authService.addRoles(response.roles);
       }
     });
+
+    this.subscriptions.push(sessionSubscription, authSubscription);
   }
 
   loginUser(): void {
     const login: Login = this.loginForm.value;
-    this.authService.signIn(login).subscribe({
+    const loginSubscription = this.authService.signIn(login).subscribe({
       next: response => {
         this.authService.addToken(response.accessToken);
         this.authService.addRoles(response.roles);
@@ -88,6 +90,12 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['']);
       }
     });
+
+    this.subscriptions.push(loginSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   protected readonly Object = Object;
--- src/frontend/ab-web-app-ui/src/app/modules/session/session.component.ts
+++ src/frontend/ab-web-app-ui/src/app/modules/session/session.component.ts
@@ -1,4 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CalendarOptions, DateSelectArg, EventClickArg, EventInput} from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import {ButtonDirective} from 'primeng/button';
 import {CalendarModule} from 'primeng/calendar';
 import {DialogModule} from 'primeng/dialog';
@@ -7,25 +12,20 @@ import {FullCalendarModule} from '@fullcalendar/angular';
 import {InputTextModule} from 'primeng/inputtext';
 import {InputTextareaModule} from 'primeng/inputtextarea';
 import {PaginatorModule} from 'primeng/paginator';
-import {MenuItem} from 'primeng/api';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {CalendarOptions, DateSelectArg, EventClickArg, EventInput} from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import {SessionService} from '../../core/service/session.service';
-import {SessionDto} from '../../core/models/session';
 import {MultiSelectModule} from 'primeng/multiselect';
 import {CurrencyPipe, DatePipe, NgClass} from '@angular/common';
-import {Subject} from '../../core/models/subject';
-import {Teacher} from '../../core/models/teacher';
+import {StepsModule} from 'primeng/steps';
+import {Ripple} from 'primeng/ripple';
+import {MenuItem} from 'primeng/api';
+import {SessionService} from '../../core/service/session.service';
 import {TeacherService} from '../../core/service/teacher.service';
 import {SubjectService} from '../../core/service/subject.service';
 import {NotificationService} from '../../core/service/notification.service';
 import {AuthService} from "../../core/service/auth.service";
-import {StepsModule} from "primeng/steps";
-import {Ripple} from "primeng/ripple";
-import {distinctUntilChanged, map, of, switchMap} from "rxjs";
+import {SessionDto} from '../../core/models/session';
+import {Subject} from '../../core/models/subject';
+import {Teacher} from '../../core/models/teacher';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-session',
@@ -48,9 +48,10 @@ import {distinctUntilChanged, map, of, switchMap} from "rxjs";
     Ripple
   ],
   templateUrl: './session.component.html',
-  styleUrls: ['./session.component.scss']
+  styleUrls: ['./session.component.scss'],
 })
-export class SessionComponent implements OnInit {
+export class SessionComponent implements OnInit, OnDestroy {
+
   calendarOptions: CalendarOptions = {};
   events: EventInput[] = [];
   showDialog = false;
@@ -63,6 +64,7 @@ export class SessionComponent implements OnInit {
   disableEdit = false;
   sessionSteps: MenuItem[] = [];
   activeStep = 0;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private fb: FormBuilder,
@@ -81,6 +83,9 @@ export class SessionComponent implements OnInit {
     this.initializeSteps();
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
   private initializeForm() {
     this.sessionForm = this.fb.group({
       startDateTime: ['', Validators.required],
@@ -94,35 +99,71 @@ export class SessionComponent implements OnInit {
     this.loadSubjects();
   }
 
-  private loadSessions(startDate: string = '', endDate: string = '') {
-    this.sessionService.getCurrentUserSessionByDateRange(startDate, endDate).subscribe(sessions => {
-      this.events = sessions.map(this.mapSessionToEvent);
-      this.updateCalendarEvents();
-    });
-  }
-
   private loadTeachers() {
-    this.sessionForm.get('subject')?.valueChanges
-    .pipe(
-      map(subject => subject.name),
-      distinctUntilChanged(),
-      switchMap(subjectName => subjectName ? this.teacherService.getTeachersBySubjectName(subjectName) : of([]))
-    )
-    .subscribe(teachers => {
-      this.teachers = teachers.map(teacher => ({
-        label: `${teacher.firstName} ${teacher.lastName}`,
-        value: teacher
-      }));
-    });
+    /*  this.subscriptions.push(
+        this.sessionForm.get('subject')?.valueChanges
+        .pipe(
+          debounceTime(300),
+          map(subject => subject?.name),
+          distinctUntilChanged(),
+          switchMap(subjectName => subjectName ? this.teacherService.getTeachersBySubjectName(subjectName) : of([]))
+        )
+        .subscribe(teachers => {
+          this.teachers = teachers.map(teacher => ({
+            label: `${teacher.firstName} ${teacher.lastName}`,
+            value: teacher
+          }));
+        });
+    )*/
+
   }
 
   private loadSubjects() {
-    this.subjectService.getCurrentUserSubject().subscribe(subjects => {
-      this.subjects = subjects.map(subject => ({
-        label: subject.name,
-        value: subject
-      }));
-    });
+    this.subscriptions.push(
+      this.subjectService.getCurrentUserSubject().subscribe(subjects => {
+        this.subjects = subjects.map(subject => ({
+          label: subject.name,
+          value: subject
+        }));
+      })
+    )
+  }
+
+  private setupCalendarOptions() {
+    this.calendarOptions = {
+      initialView: 'dayGridMonth',
+      plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      editable: false,
+      selectable: true,
+      selectMirror: true,
+      dayMaxEvents: true,
+      eventClick: this.onEventClick.bind(this),
+      select: this.onDateSelect.bind(this),
+      events: this.events,
+      datesSet: this.onDateRangeChange.bind(this)
+    };
+  }
+
+  private initializeSteps() {
+    this.sessionSteps = [
+      {label: 'Choose Subject & Time'},
+      {label: 'Choose Teacher'}
+    ];
+  }
+
+  private loadSessions(startDate: string = '', endDate: string = '') {
+    this.subscriptions.push(
+      this.sessionService.getCurrentUserSessionByDateRange(startDate, endDate).subscribe(sessions => {
+        this.events = sessions.map(this.mapSessionToEvent);
+        this.updateCalendarEvents();
+      })
+    )
+
   }
 
   private mapSessionToEvent(session: SessionDto): EventInput {
@@ -144,31 +185,18 @@ export class SessionComponent implements OnInit {
     };
   }
 
-  private setupCalendarOptions() {
-    this.calendarOptions = {
-      initialView: 'dayGridMonth',
-      plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      editable: false,
-      selectable: true,
-      selectMirror: true,
-      dayMaxEvents: true,
-      eventClick: (e: EventClickArg) => this.onEventClick(e),
-      select: (e: DateSelectArg) => this.onDateSelect(e),
-      events: this.events,
-      datesSet: (dateInfo) => this.onDateRangeChange(dateInfo)
-    };
+  private updateCalendarEvents() {
+    if (this.calendarOptions.events !== this.events) {
+      this.calendarOptions = {...this.calendarOptions, events: this.events};
+    }
   }
 
-  private initializeSteps() {
-    this.sessionSteps = [
-      {label: 'Choose Subject & Time'},
-      {label: 'Choose Teacher'}
-    ];
+  private onDateRangeChange(viewInfo: any) {
+    this.loadSessions(viewInfo.startStr, viewInfo.endStr);
+  }
+
+  onEditClick() {
+    this.view = 'edit';
   }
 
   nextStep() {
@@ -194,48 +222,43 @@ export class SessionComponent implements OnInit {
     return false;
   }
 
+  isNextDisabled(): boolean {
+    return this.activeStep === 0 && this.isFirstStepInvalid();
+  }
+
+  isLastStep(): boolean {
+    return this.activeStep === this.sessionSteps.length - 1;
+  }
+
   handleSave() {
     if (this.sessionForm.invalid) {
       return;
     }
 
-    if (this.selectedSession) {
-      this.updateSession();
-    } else {
-      this.addNewSession();
-    }
-
+    this.selectedSession ? this.updateSession() : this.addNewSession();
     this.showDialog = false;
     this.resetEvent();
   }
 
   private addNewSession() {
-    this.sessionService.save(this.sessionForm.value).subscribe(() => {
-      this.notificationService.showSuccess('Session saved successfully');
-      this.loadSessions();
-    });
+    this.subscriptions.push(
+      this.sessionService.save(this.sessionForm.value).subscribe(() => {
+        this.notificationService.showSuccess('Session saved successfully');
+        this.loadSessions();
+      })
+    )
+
   }
 
   private updateSession() {
-    if (this.selectedSession) {
-      const updatedSession = {...this.selectedSession, ...this.sessionForm.value};
+    const updatedSession = {...this.selectedSession, ...this.sessionForm.value};
+    this.subscriptions.push(
       this.sessionService.update(updatedSession).subscribe(() => {
         this.notificationService.showSuccess('Session updated successfully');
         this.loadSessions();
-      });
-    }
-  }
+      })
+    )
 
-  private updateCalendarEvents() {
-    this.calendarOptions = {...this.calendarOptions, events: this.events};
-  }
-
-  private onDateRangeChange(viewInfo: any) {
-    this.loadSessions(viewInfo.startStr, viewInfo.endStr);
-  }
-
-  onEditClick() {
-    this.view = 'edit';
   }
 
   resetEvent() {
@@ -248,57 +271,49 @@ export class SessionComponent implements OnInit {
     return !!(control?.invalid && (control?.dirty || control?.touched));
   }
 
-  isLastStep(): boolean {
-    return this.activeStep === this.sessionSteps.length - 1;
-  }
-
-  isNextDisabled(): boolean {
-    if (this.activeStep === 0) {
-      return this.isFirstStepInvalid();
-    }
-    return false;
-  }
-
-  private onEventClick(e: EventClickArg) {
+  private onEventClick(eventClick: EventClickArg) {
     if (this.authService.hasRoles(["TEACHER"])) {
       return;
     }
 
-    this.selectedSession = {
-      id: parseInt(e.event.id),
-      status: '',
-      title: e.event.title || '',
-      startDateTime: e.event.start || new Date(),
-      endDateTime: e.event.end || new Date(),
-      description: e.event.extendedProps['description'] || '',
-      meetingLink: e.event.extendedProps['meetingLink'] || '',
-      price: e.event.extendedProps['price'] || 0,
-      duration: e.event.extendedProps['duration'] || 0,
-      teacher: e.event.extendedProps['teacher'],
-      subject: e.event.extendedProps['subject'],
-      createdDate: e.event.extendedProps['createdDate']
-    };
-
+    this.selectedSession = this.mapEventToSession(eventClick);
     this.title = this.selectedSession.title;
     this.disableEditIfNecessary();
     this.view = 'display';
     this.showDialog = true;
   }
 
+  private mapEventToSession(eventClick: EventClickArg): SessionDto {
+    return {
+      id: parseInt(eventClick.event.id),
+      status: '',
+      title: eventClick.event.title || '',
+      startDateTime: eventClick.event.start || new Date(),
+      endDateTime: eventClick.event.end || new Date(),
+      description: eventClick.event.extendedProps['description'] || '',
+      meetingLink: eventClick.event.extendedProps['meetingLink'] || '',
+      price: eventClick.event.extendedProps['price'] || 0,
+      duration: eventClick.event.extendedProps['duration'] || 0,
+      teacher: eventClick.event.extendedProps['teacher'],
+      subject: eventClick.event.extendedProps['subject'],
+      createdDate: eventClick.event.extendedProps['createdDate']
+    };
+  }
+
   private disableEditIfNecessary() {
     const createdDate = this.selectedSession?.createdDate;
     const currentDate = new Date();
-    if (createdDate)
-      this.disableEdit = createdDate && createdDate < currentDate;
+
+    // Implement logic for disabling edit based on the createdDate
   }
 
   private onDateSelect(selectInfo: DateSelectArg) {
-    this.resetEvent();
-    this.showDialog = true;
-    this.view = 'new';
-    this.title = 'New Event';
-    this.sessionForm.patchValue({
-      startDateTime: selectInfo.startStr
-    });
+    console.log("hh")
+    //this.resetEvent();
+    // this.title = 'New Session';
+    //this.view = 'new';
+    //this.showDialog = true;
+    //this.sessionForm.patchValue({startDateTime: selectInfo.start});
   }
+
 }
