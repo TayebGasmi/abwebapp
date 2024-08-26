4983b742c942277007097d5f99e6e825f8644da5
trabelsifiras <firas.trabelsi.1@esprit.tn>
2024-08-26 16:49:48 +0200

    landingsession

--- src/frontend/ab-web-app-ui/src/app/core/guard/auth.guard.ts
+++ src/frontend/ab-web-app-ui/src/app/core/guard/auth.guard.ts
@@ -1,9 +1,11 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {AuthService} from "../service/auth.service";
+import {TeacherService} from "../service/teacher.service";
 
 export const authGuard: CanActivateFn = (route) => {
   const authService = inject(AuthService);
+  const teacherService:TeacherService=inject(TeacherService)
   const router = inject(Router);
   const expectedRoles = route.data["roles"] || [];
   if (!authService.isAuthenticated()) {
@@ -16,6 +18,10 @@ export const authGuard: CanActivateFn = (route) => {
   }
   if (!authService.isProfileComplete()){
     router.navigate(['/complete'])
+    return false
+  }
+  if(!teacherService.isConfirmedByAdmin().subscribe(teacher=>teacher.confirmedByAdmin)){
+    router.navigate(['/auth/login'])
     return false
   }
 
--- src/frontend/ab-web-app-ui/src/app/core/service/teacher.service.ts
+++ src/frontend/ab-web-app-ui/src/app/core/service/teacher.service.ts
@@ -1,8 +1,11 @@
-import {Injectable} from '@angular/core';
+import {inject, Inject, Injectable} from '@angular/core';
 import {BaseService} from "./base.service";
 import {Teacher} from "../models/teacher";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
+import {AuthService} from "./auth.service";
+import {BrowserStorageService} from "./browser-storage.service";
+import {Observable, of} from "rxjs";
 
 const baseUrl = environment.APPOINTMENT_BOOKING_URL;
 
@@ -10,9 +13,18 @@ const baseUrl = environment.APPOINTMENT_BOOKING_URL;
   providedIn: 'root'
 })
 export class TeacherService extends BaseService<Teacher, number> {
-
+  authService = inject(AuthService);
+  browserStorage:BrowserStorageService =inject(BrowserStorageService);
+  teacher!:Teacher ;
   constructor(http: HttpClient) {
     const url = `${baseUrl}/teacher`;
     super(http, url);
+  }
+
+  isConfirmedByAdmin():Observable<Teacher>{
+    if(this.authService?.isAuthenticated() && this.authService.hasRoles(["TEACHER"])) {
+      return this.findById(JSON.parse(<string>this.browserStorage?.getItem("user"))["id"]);
+    }
+    return of(this.teacher);
   }
 }
