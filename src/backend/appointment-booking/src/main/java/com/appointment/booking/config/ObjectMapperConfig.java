b85b55c974f26e15b730e64ee6666e0d096d5cea
tayeb <tayebgasmi1999@gmail.com>
2024-09-30 13:33:20 +0100

    add java 8 time support to object mapper

--- src/backend/appointment-booking/src/main/java/com/appointment/booking/config/ObjectMapperConfig.java
+++ src/backend/appointment-booking/src/main/java/com/appointment/booking/config/ObjectMapperConfig.java
@@ -2,6 +2,8 @@ package com.appointment.booking.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,8 @@ public class ObjectMapperConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
 
     }
