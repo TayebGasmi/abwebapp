c0e30316be9bc6b804d3d27f7456b8639393baec
tayeb <tayebgasmi1999@gmail.com>
2024-10-02 23:10:49 +0100

    web socket config

--- src/backend/appointment-booking/src/main/java/com/appointment/booking/config/WebSocketConfig.java
+++ src/backend/appointment-booking/src/main/java/com/appointment/booking/config/WebSocketConfig.java
@@ -1,15 +1,36 @@
 package com.appointment.booking.config;
 
+import static com.appointment.booking.utils.WsConstants.WS_TOPICS;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+@Slf4j
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final String[] wsAllowedOrigins;
+
+    public WebSocketConfig(@Value("${websocket.allowed.origins}") String[] wsAllowedOrigins) {
+        this.wsAllowedOrigins = wsAllowedOrigins;
+    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").
+            setAllowedOriginPatterns(wsAllowedOrigins);
     }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker(WS_TOPICS);
+        registry.setApplicationDestinationPrefixes("/ws");
+    }
+
 }
